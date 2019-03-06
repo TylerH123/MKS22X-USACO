4af3f8a651827a99e5e92a04ba8ab8c03e84b1b1
Tyler Huang <thuang00@stuy.edu>
2019-03-06 17:28:00 -0500

    working on bronze

--- USACO.java
+++ USACO.java
@@ -8,9 +8,24 @@ public class USACO{
     int R,C,E,N;
     int[][] lake;
     boolean firstLine = true;
-    while (file.hasNextLine()){
+    String[] g = new String[4];
+    if (file.hasNextLine()){
+      int cols = 0;
       String line = file.nextLine();
+      //split the first line by space then copy which get copied into an array of String
       if (firstLine){
+        g = line.split(" ");
+        firstLine = false;
+      }
+    }
+    R = Integer.parseInt(g[0]);
+    C = Integer.parseInt(g[1]);
+    E = Integer.parseInt(g[2]);
+    N = Integer.parseInt(g[3]);
+    lake = new int[R][C];
+    file = new Scanner(f);
+    //copies the the lake elevations into the lake array
+      for (int r = 1; r < R - 1; r++){
         for (int i = 0; i < line.length(); i++){
 
         }
@@ -20,6 +35,11 @@ public class USACO{
   }
 
   public static void main(String[] args){
-
+    try{
+      bronze("makelake.in ");
+    }
+    catch(FileNotFoundException e){
+      System.out.println("File not found");
+    }
   }
 }
