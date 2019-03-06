ed895e78cfd5a69e1368395c53a908caf64335c6
Tyler Huang <thuang00@stuy.edu>
2019-03-06 14:36:17 -0500

    doing bronzE

--- USACO.java
+++ USACO.java
@@ -1,5 +1,25 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
+    File f = new File(filename);
+    Scanner file = new Scanner(f);
+    int R,C,E,N;
+    int[][] lake;
+    boolean firstLine = true;
+    while (file.hasNextLine()){
+      String line = file.nextLine();
+      if (firstLine){
+        for (int i = 0; i < line.length(); i++){
+
+        }
+      }
+    }
     return 1;
+  }
+
+  public static void main(String[] args){
+
   }
 }
