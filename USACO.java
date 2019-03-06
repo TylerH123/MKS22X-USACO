950571bc5f42ce506fed3eb5cc926e0b0887cbc4
Tyler Huang <thuang00@stuy.edu>
2019-03-06 18:06:12 -0500

    doing stomp

--- USACO.java
+++ USACO.java
@@ -5,16 +5,14 @@ public class USACO{
   public static int bronze(String filename) throws FileNotFoundException{
     File f = new File(filename);
     Scanner file = new Scanner(f);
-    int R,C,E,N;
+    int R,C,E,N,R_s,C_s,D_s,volume;
     int[][] lake;
     boolean firstLine = true;
     String[] g = new String[4];
     if (file.hasNextLine()){
-      int cols = 0;
-      String line = file.nextLine();
       //split the first line by space then copy which get copied into an array of String
       if (firstLine){
-        g = line.split(" ");
+        g = file.nextLine().split(" ");
         firstLine = false;
       }
     }
@@ -23,15 +21,44 @@ public class USACO{
     E = Integer.parseInt(g[2]);
     N = Integer.parseInt(g[3]);
     lake = new int[R][C];
-    file = new Scanner(f);
+    int row = 0;
     //copies the the lake elevations into the lake array
-      for (int r = 1; r < R - 1; r++){
-        for (int i = 0; i < line.length(); i++){
-
+    while(file.hasNextLine()){
+      String line = file.nextLine();
+      if (row < R){
+        String[] elev = line.split(" ");
+        int col = 0;
+        for(String item : elev){
+          lake[row][col] = Integer.parseInt(item);
+          col++;
         }
+        row++;
+      }
+      if (row > R){
+        String[] dir = line.split(" ");
+        R_s = Integer.parseInt(dir[0]);
+        C_s = Integer.parseInt(dir[1]);
+        D_s = Integer.parseInt(dir[2]);
+        stomp(lake,R_s,C_s,D_s);
       }
     }
+    //tested the copying the elevation
+    //for (int i = 0; i < lake.length; i++){
+    //  for (int j = 0; j < lake[i].length; j++){
+    //    System.out.println(lake[i][j]);
+    //  }
+    //}
     return 1;
+  }
+
+  public static void stomp(int[][] lake, int r, int c, int d){
+    int biggest = 0;
+    //finding highest elevation
+    for (int row = 0; row < 3; row++){
+      for(int col = 0; col < 3; col++){
+        lake[r+row][c+col] = 3;
+      }
+    }
   }
 
   public static void main(String[] args){
